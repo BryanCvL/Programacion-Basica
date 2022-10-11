8a45d6445f0bc2d3b003c9f0c4040cec38500e24
Bryan Carvajal <brayan.carvajal@utp.ac.pa>
2022-10-10 22:12:57 -0500

    Clase 77

--- js/mokepon.js
+++ js/mokepon.js
@@ -25,6 +25,7 @@ const contenedorAtaques = document.getElementById('contenedor-ataques')
 const sectionVerMapa = document.getElementById('ver-mapa')
 const mapa = document.getElementById('mapa')
 
+let jugadorId = null
 let ataqueJugador = []
 let ataqueEnemigo = []
 let vidasJugador = 0
@@ -220,6 +221,7 @@ function unirseAlJuego(){
                 res.text()
                     .then(function(respuesta){
                         console.log(respuesta)
+                        jugadorId = respuesta
                     })
             }
         })
@@ -299,12 +301,26 @@ function selecionarMascotaJugador(){
     }
     else {alert('Debes seleccionar una mascota')}
 
+    seleccionarMokepon(mascotaJugador)
+
     extraerAtaques(mascotaJugador)
 
     sectionSeleccionarMascota.style.display = 'none'
     
     sectionVerMapa.style.display = 'flex'
     iniciarMapa()
+}
+
+function seleccionarMokepon(mascotaJugador) {
+    fetch(`http://localhost:8080/mokepon/${jugadorId}`, {
+        method: "post",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            mokepon: mascotaJugador
+        })
+    })
 }
 
 function extraerAtaques(mascotaJugador) {
